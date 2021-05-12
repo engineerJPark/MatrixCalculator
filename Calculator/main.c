38ae2a2b43a75a3a53b12cf25e463de9a8a277fb
박준성 <jshackist@gmail.com>
2021-05-12 14:52:45 +0000

    0512 edited

--- Calculator/main.c
+++ Calculator/main.c
@@ -1,24 +1,13 @@
 /*
-해결된 것
-getMatrix에서 행,열 크기 제한 걸기*****
-showMatrix 함수 만들기*****
-initializematrix를 getMatrix 안에 넣기*****
-
 해결해야할 것
-transpose과정에서 메모리 관리를 위해 killMatrix를 만들자
-아 근데 소수점은 처리 어떻게 하지.
-아.
-하는데 너무 괴로우므로 reduceMatrix까지만 구현하기로...
-나머지는 추후에 더 개발하도록 하자.
-
+소수점 처리하기 : .감지시 .이후의 숫자 세는 counter를 사용.
+문자 입력 모듈을 따로 함수로 빼내기. 코드가 너무 더러움.
 */
 
 #include<stdio.h>
 #define TEXT_LENGTH 512
 
-//은 행렬의 주소
-//*matData은 행렬의 하나의 행을 지정
-//**matData은 행렬의 하나의 원소(열)를 지정한다.
+//matData은 행렬의 주소, *matData은 행렬의 하나의 행을 지정, **matData은 행렬의 하나의 원소(열)를 지정한다.
 typedef struct Matrix{
 	int n, m = 0;	//행렬의 행과 열의 크기
 	char ** matData;	//행렬의 data
@@ -30,18 +19,12 @@ void getMatrix(Matrix matStruct);
 void showMatrix(Matrix matStruct);
 void editMatrix(Matrix matStruct);
 
+void killData(Matrix matStruct);
 void transposeMatrix(Matrix matStruct);
-void reduceMatrix(Matrix matStruct);
-// void inverseMatrix(Matrix matStruct);
-// void determinantMatrix(Matrix matStruct);
-// void addMatrix(Matrix matStruct);
-// void multMatrix(Matrix matStruct);
-// void eigenvalueMatrix(Matrix matStruct);
-// void eigenvectorMatrix(Matrix matStruct);
 
 void main(){
 	//생성된 행렬 개수를 세는 변수.
-	i = 0;
+	int i = 0;
 	//행렬구조체의 주소가 저장되는 곳. 배열로 쉽게 접근할 목적이다.
 	Matrix *matBox;
 	//초기 행렬 초기화. 초기 모든 행렬 원소가 0이 되도록 한다.
@@ -55,26 +38,14 @@ void main(){
 	//초기행렬 입력
 	getMatrix(matBox[i]);
 	showMatrix(matBox[i]);
-
-	//작동 중에 해야할것
-	while(1){
-		//행렬수정
-		
-		//행렬추가
-
-		//전치
-
-		//리듀스
-
-	}
 }
 
 //5x5행렬을 전부 0으로 초기화한다.
 void initializeMatrix(Matrix matStruct){
 	//이제 행과 열을 모두 0으로 초기화한다.
 	matStruct.matData = (char**)malloc(sizeof(char*) * matStruct.n);
-	for(i = 0; i < n; i++){
-		matStruct.matData[i] = (int*)calloc(m, sizeof(int) * matStruct.m);
+	for(int i = 0; i < matStruct.n; i++){
+		matStruct.matData[i] = (float*)calloc(matStruct.m, sizeof(float) * matStruct.m);
 	}
 }
 
@@ -88,7 +59,7 @@ void getMatrix(Matrix matStruct){
 	
 	initializeMatrix(matStruct);
 
-	matStruct.textMat = (char)malloc(TEXT_LENGTH);
+	matStruct.textMat = (char*)malloc(TEXT_LENGTH);
 	scanf("%s", matStruct.textMat);
 	char len = strlen(matStruct.textMat);
 	
@@ -99,23 +70,27 @@ void getMatrix(Matrix matStruct){
 	
 	//문자를 행과 열로 나눠준다.
 	for(int i = 0; i <= len; i++){
-		if(matStruct.textMat[i]!=","){//숫자 입력. 10진수 단위로 또 만들어줘야한다.
+		if(matStruct.textMat[i]!="," || matStruct.textMat[i]!="." || matStruct.textMat[i]!=NULL){
+			//숫자 입력. 10진수 단위로 또 만들어줘야한다.
 			numtemp[z] = atoi(matStruct.textMat[i]);
 			z++;
 		}
-		elseif(matStruct.textMat[i]==","||matStruct.textMat[i]==NULL){//쉼표나 마지막표시(널값) 들어온 경우 
-			for(j=1;j<=z;j++){//z-j번씩 10을 곱해준다음 더해준다. 된다.
-				for(k=1;k<=(z-j);k++){
-					num = num + numtemp[j]*10;
+		else if(matStruct.textMat[i]==","||matStruct.textMat[i]==NULL){
+			//쉼표나 마지막표시(널값) 들어온 경우 
+			for(j=1;j <= z;j++){
+				//z-j번씩 10을 곱해준다음 더해준다.
+				for(k=1;k<=(z - j);k++){
+					numtemp[j] = numtemp[j]*10;
 				}
+				num = num + numtemp[j];
 			}
 			matStruct.matData[x][y] = num;	//정확한 위치에 num을 대입
 			num = 0;
-			y++; if(y > m){y=0; x++;} //열 혹은 행 증가
+			y++; if(y > matStruct.m){y=0; x++;} //열 혹은 행 증가
 		}
 		else{
 			printf("Error 001");
-			printf("숫자와 쉼표 외에는 입력하지 마십시오.")
+			printf("숫자와 쉼표 외에는 입력하지 마십시오.");
 			return -1;
 		}
 		//한 원소의 입력 후 주요 변수 초기화 작업
@@ -140,16 +115,16 @@ void showMatrix(Matrix matStruct){
 
 void editMatrix(Matrix matStruct){
 	printf("%s\n", matStruct.textMat);
-	printf("복붙해서 수정하면 쉽지않을까...?")
 	getMatrix(matStruct);
 }
 
 void transposeMatrix(Matrix matStruct){
-	temp = matStruct.n;
+	int temp = matStruct.n;
 	matStruct.n = matStruct.m;
 	matStruct.m = temp;
 	
 	// killMatrix 부분
+	killData(matStruct);
 	
 	// getMatrix와 비슷하지만 textMat을 그대로 사용해서 처리
 	initializeMatrix(matStruct);
@@ -162,15 +137,17 @@ void transposeMatrix(Matrix matStruct){
 	
 	//문자를 행과 열로 나눠준다.
 	for(int i = 0; i <= len; i++){
-		if(matStruct.textMat[i]!=","){//숫자 입력. 10진수 단위로 또 만들어줘야한다.
+		if(matStruct.textMat[i]!="," || matStruct.textMat[i]!="." || matStruct.textMat[i]!=NULL){//숫자 입력. 10진수 단위로 또 만들어줘야한다.
 			numtemp[z] = atoi(matStruct.textMat[i]);
 			z++;
 		}
 		elseif(matStruct.textMat[i]==","||matStruct.textMat[i]==NULL){//쉼표나 마지막표시(널값) 들어온 경우 
-			for(j=1;j<=z;j++){//z-j번씩 10을 곱해준다음 더해준다. 된다.
+			for(j=1;j<=z;j++){
+				//z-j번씩 10을 곱해준다음 더해준다.
 				for(k=1;k<=(z-j);k++){
-					num = num + numtemp[j]*10;
+					numtemp[j] = numtemp[j]*10;
 				}
+				num = num + numtemp[j];
 			}
 			matStruct.matData[x][y] = num;	//정확한 위치에 num을 대입
 			num = 0;
@@ -190,14 +167,9 @@ void transposeMatrix(Matrix matStruct){
 	}
 }
 
-void reduceMatrix(Matrix matStruct){
-	// 0행이 있는지 확인한다.
-	// 0이 선행성분으로 있는 행을 아래로 내린다.
-	// 첫 행 선행성분 1이 되게 나눈다.
-	// 첫 행 선행성분 위아래로 모두 0이 되게 한다.
-	// 다음행 선행성분 1이 되게 나누다.
-	// 다음행 선행성분 위아래로 모두 0이 되게 한다.
-	// 반복
-	// 마지막 행 or 선행성분이 없는 경우까지 시행.
-	
+void killData(Matrix matStruct){
+	for (int i = 0; i < matStruct.n; i++){
+		free(matStruct.matData[i]);
+	}
+	free(matStruct.matData);
 }
