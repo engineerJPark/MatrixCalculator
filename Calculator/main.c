668f988e7255210457cdd420a1030619ce85bfdc
박준성 <jshackist@gmail.com>
2021-05-26 03:44:14 +0000

    Call by dereference로 수정함

--- Calculator/main.c
+++ Calculator/main.c
@@ -19,29 +19,29 @@ typedef struct Matrix{
 } Matrix;
 
 //5x5행렬을 전부 0으로 초기화한다.
-void initializeMatrix(Matrix matStruct){
+void initializeMatrix(Matrix * matStruct){
 	//이제 행과 열을 모두 0으로 초기화한다.
-	matStruct.matData = (char**)malloc(sizeof(char**) * matStruct.n);
-	for(int i = 0; i < matStruct.n; i++){
-		matStruct.matData[i] = (float*)calloc(matStruct.m, sizeof(float) * matStruct.m);
+	matStruct->matData = (char**)malloc(sizeof(char**) * matStruct->n);
+	for(int i = 0; i < matStruct->n; i++){
+		matStruct->matData[i] = (float*)calloc(matStruct->m, sizeof(float) * matStruct->m);
 	}
 }
 
 //text형태로 행렬을 받는다. ','로 숫자를 구분한다.
-void getMatrix(Matrix matStruct){
+void getMatrix(Matrix * matStruct){
 	int n, m = 0;
 	printf("행의 크기 n, 열의 크기 m을 차례로 입력하세요.");
 	scanf("%d", &n); scanf("%d", &m);
 	if(n > 5 || m > 5){ printf("본 계산기는 5x5까지만 지원합니다.\n"); return -1;}
-	matStruct.n = n; matStruct.m = m;	
+	matStruct->n = n; matStruct->m = m;	
 	
 	initializeMatrix(matStruct);
 
-	matStruct.textMat = (char*)malloc(TEXT_LENGTH);
+	matStruct->textMat = (char*)malloc(TEXT_LENGTH);
 	printf("데이터를 입력해주세요.");
-	scanf("%s", matStruct.textMat);
+	scanf("%s", matStruct->textMat);
 	 
-	char len = strlen(matStruct.textMat);
+	char len = strlen(matStruct->textMat);
 	char num = 0;	//숫자를 임시로 저장할 공간이다.
 	char numtemp[10] = {0}; //숫자를 임시로 저장할 공간이다.
 	char z = 0, k = 1;
@@ -50,7 +50,7 @@ void getMatrix(Matrix matStruct){
 	// 문자를 행과 열로 나눠준다.
 	// 이거야 말로 그냥 atoi() 쓰면 되는데 왜 이렇게?
 	for(int i = 0; i <= len; i++){
-		if((matStruct.textMat[i] == ',') || (matStruct.textMat[i] == NULL)){
+		if((matStruct->textMat[i] == ',') || (matStruct->textMat[i] == NULL)){
 			//쉼표나 마지막표시(널값) 들어온 경우
 			for(int j = 1; j <= z; j++){
 				//z-j번씩 10을 곱해준다음 더해준다.
@@ -59,13 +59,13 @@ void getMatrix(Matrix matStruct){
 				}
 				num = num + numtemp[j - 1];
 			}
-			matStruct.matData[x][y] = num;	//정확한 위치에 num을 대입
+			matStruct->matData[x][y] = num;	//정확한 위치에 num을 대입
 			num = 0;
-			y++; if(y > matStruct.m){y=0; x++;} //열 혹은 행 증가
+			y++; if(y > matStruct->m){y=0; x++;} //열 혹은 행 증가
 		}
 		else{
 			//숫자 입력. 10진수 단위로 또 만들어줘야한다.
-			numtemp[z] = matStruct.textMat[i]-48; // 단일 문자를 알맞은 숫자로 변환시켜주어야한다.
+			numtemp[z] = matStruct->textMat[i]-48; // 단일 문자를 알맞은 숫자로 변환시켜주어야한다.
 			z++;
 		}
 		//한 원소의 입력 후 주요 변수 초기화 작업
@@ -80,36 +80,36 @@ void getMatrix(Matrix matStruct){
 void showMatrix(Matrix matStruct){
 	int i = 0, j = 0; // 각각 행과 열 상징
 	printf('\n');
-	for(i = 0; i < matStruct.n; i++){
-		for(j = 0; j < matStruct.m; j++){
-			printf("%d ", matStruct.matData[i][j]);
+	for(i = 0; i < matStruct->n; i++){
+		for(j = 0; j < matStruct->m; j++){
+			printf("%d ", matStruct->matData[i][j]);
 		}
 		printf('\n');
 	}
 }
 
 void editMatrix(Matrix matStruct){
-	printf("%s\n", matStruct.textMat);
+	printf("%s\n", matStruct->textMat);
 	getMatrix(matStruct);
 }
 
 
 void killData(Matrix matStruct){
-	for (int i = 0; i < matStruct.n; i++){
-		free(matStruct.matData[i]);
+	for (int i = 0; i < matStruct->n; i++){
+		free(matStruct->matData[i]);
 	}
-	free(matStruct.matData);
+	free(matStruct->matData);
 }
 
 int main(){
 	//생성된 행렬 개수를 세는 변수.
 	int i = 0;
 	//행렬구조체의 주소가 저장되는 곳. 배열로 쉽게 접근할 목적이다.
-	Matrix matBox[MATMAXNUMBER];
+	Matrix * matBox[MATMAXNUMBER];
 	//초기 행렬 초기화. 초기 모든 행렬 원소가 0이 되도록 한다.
 	Matrix initMat;
 	initMat.n = initMat.m = 2;
-	matBox[i] = initMat;
+	matBox[i] = &initMat;
 	//안내문
 	printf("최대 5x5 행렬을 지원합니다.\n\n");
 	printf("행렬의 값을 텍스트로 입력해주세요. ','는 열 방향에서 왼쪽으로 오른쪽으로 하나씩 개행시킵니다.\n");
